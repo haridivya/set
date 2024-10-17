399ff3e1593b5989fd998c1a87f8a78a0473cbad
haridivya <123063502+haridivya@users.noreply.github.com>
2024-10-17 14:53:15 +0530

    similar_elements.py

--- similar_elements.py
+++ similar_elements.py
@@ -7,3 +7,10 @@ Sample Output:
 2 4 
 Note: Trailing spaces are there at the end of the output.
 '''
+set1,set2=input().split(' '),input().split(' ')
+values1,values2=set(set1),set(set2)
+temp=''
+for i in values1:
+  if i in values2:
+    temp+=i+' '
+print(temp)
