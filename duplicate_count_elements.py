7f5ab7637ef487ddc135f6d1a37909df8d92643c
haridivya <123063502+haridivya@users.noreply.github.com>
2024-10-17 13:56:40 +0530

    duplicate_count_elements.py

--- duplicate_count_elements.py
+++ duplicate_count_elements.py
@@ -12,3 +12,12 @@ Sample Output:
 Duplicate Values: 3
 1 2 3 
 '''
+n=int(input())
+value=[int(input()) for i in range(0,n)]
+elements=set(value)
+duplicate_values=len(value)-len(elements)
+print(f"Duplicate Values: {duplicate_values}")
+str1=''
+for j in elements:
+    str1+=str(j)+' '
+print(str1)
