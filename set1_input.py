9602c865418ad32d91893edf439e8dbedc8263fb
haridivya <123063502+haridivya@users.noreply.github.com>
2024-10-17 12:07:09 +0530

    Create set1_input.py

---
+++ set1_input.py
@@ -0,0 +1,8 @@
+'''
+ Write a program to get input in a single line separated by space and print the values of set in single line separated by space.
+Sample Input:
+3 1 5 4 2
+Sample Output:
+1 2 3 4 5
+Note: There is no trailing space at the end of output.
+'''
