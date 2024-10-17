f669ddcd99c16d1bc3f25b97a1c001d1cca3b25c
haridivya <123063502+haridivya@users.noreply.github.com>
2024-10-17 12:11:44 +0530

    Create similar_elements.py

---
+++ similar_elements.py
@@ -0,0 +1,9 @@
+'''
+Write a program to print the values which are similar in both given sets.
+Sample Input:
+1 2 3 4
+2 4 6 8
+Sample Output:
+2 4 
+Note: Trailing spaces are there at the end of the output.
+'''
