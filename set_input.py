cdb652e61c6df8c0d2e8b8f26ca3c41b4db357d3
haridivya <123063502+haridivya@users.noreply.github.com>
2024-10-17 14:41:06 +0530

    set_input.py

--- set_input.py
+++ set_input.py
@@ -12,3 +12,12 @@ Sample Output:
 1 2 3 4 5 
 Note: There is trailing space at the end of output
 '''
+no_input=int(input())
+temp=''
+set1=[]
+for i in range(0,no_input):
+    set1.append(int(input())) 
+set1.sort()
+for i in set1:
+    temp+=str(i)+' '
+print(temp)
