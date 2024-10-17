2775f3a2bf225dafc050204c59eeb3ab2c3e5dec
haridivya <123063502+haridivya@users.noreply.github.com>
2024-10-17 14:21:37 +0530

    set1_input.py

--- set1_input.py
+++ set1_input.py
@@ -6,3 +6,11 @@ Sample Output:
 1 2 3 4 5
 Note: There is no trailing space at the end of output.
 '''
+input_elements=input().split(' ')
+input_values=[int(i) for i in input_elements]
+input_values.sort()
+set1=set(input_values)
+temp=''
+for i in set1:
+    temp+=str(i)+' '
+print(temp)
