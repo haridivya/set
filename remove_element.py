ff1da6c5619e18dc782c6193c85fcc6fca007e30
haridivya <123063502+haridivya@users.noreply.github.com>
2024-10-17 14:10:44 +0530

    Update remove_element.py

--- remove_element.py
+++ remove_element.py
@@ -7,3 +7,12 @@ Sample Output:
 1 3 4 
 Note: There is a trailing space at the end of the list.
 '''
+elements=input().split(' ')
+delete_ele=int(input())
+set_value=[int(i) for i in elements]
+set_value.remove(delete_ele)
+str1=''
+for i in set_value:
+    str1+=str(i)+' '
+print(str1)
+
