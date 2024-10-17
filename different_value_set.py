aeb7f3741e03ac3067848a6bc37dc6e828c4a866
haridivya <123063502+haridivya@users.noreply.github.com>
2024-10-17 12:08:13 +0530

    Create different_value_set.py

---
+++ different_value_set.py
@@ -0,0 +1,9 @@
+'''
+Write a program to print only the different values between two given sets.
+Sample Input:
+1 2 3 4
+2 4 6 8
+Sample Output:
+1 3
+Note: There are trailing spaces at the end of output.
+'''
