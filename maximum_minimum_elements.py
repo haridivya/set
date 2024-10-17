5eeebdf56ea07e69b06d988d279f534decee149a
haridivya <123063502+haridivya@users.noreply.github.com>
2024-10-17 14:03:25 +0530

    maximum_minimum_elements.py

--- maximum_minimum_elements.py
+++ maximum_minimum_elements.py
@@ -6,3 +6,7 @@ Sample Output:
 Maximum: 5
 Minimum: 1
 '''
+set_elements=input().split(' ')
+values=[int(i) for i in set_elements]
+print(f"Maximum: {max(values)}")
+print(f"Minimum: {min(values)}")
